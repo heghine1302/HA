1d606e44b47b073a0e135639a1669f60cf100938
Heghine Aghajanyan <50370257+heghine1302@users.noreply.github.com>
2019-05-11 11:27:21 +0400

    Add files via upload

---
+++ Palindrome.cpp
@@ -0,0 +1,29 @@
+﻿//3.Program to check whether a number is palindrome or not.
+
+#include <iostream>
+using namespace std;
+int main() {
+
+	int num, N, h=0, m;
+	//h-hakarak, m-mnacord.
+	cin >> num;
+	N = num; //Հետո օգտագործելու համար։
+	do {
+		m = num % 10;
+		h = h * 10 + m; 
+		num = num / 10;
+	}
+	while (num != 0);
+
+	if (h == N) 
+	{
+		cout << N << " is palindrome." << endl;
+	}
+	else
+	{
+		cout << "Error" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
